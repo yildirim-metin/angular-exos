8c4cfb913bc85c5a8d7b74ecf7d6144eaff814fe
yildirim-metin <metin_yildirim@outlook.be>
2025-11-07 13:46:28 +0100

    replace add component by form-product

--- src/app/features/exercise/pages/exercise-service-injection/exercise-service-injection.component.ts
+++ src/app/features/exercise/pages/exercise-service-injection/exercise-service-injection.component.ts
@@ -1,17 +1,16 @@
 import {Component, inject} from "@angular/core";
 import {ExerciseHeaderComponent} from "@app/features/exercise/components/layout/exercise-header/exercise-header.component";
 import {SpoilComponent} from "@shared/components/spoil/spoil.component";
-// import {AddComponent} from "./components/add/add.component";
 import {ListComponent} from "./components/list/list.component";
 import {ProductService} from "./services/product-service";
 import {FormProductComponent} from "./components/form-product/form-product.component";
+import {Product} from "./models/product";
 
 @Component({
 	selector: "exercise-service-injection",
 	imports: [
 		ExerciseHeaderComponent,
 		SpoilComponent,
-		// AddComponent,
 		ListComponent,
 		FormProductComponent,
 	],
@@ -24,6 +23,10 @@ export class ExerciseServiceInjectionComponent {
 	private readonly _productService = inject(ProductService);
 
 	products = this._productService.products;
+
+	addProduct(product: Product) {
+		this._productService.add(product);
+	}
 
 	addItem(index: number) {
 		this._productService.increment(index);
